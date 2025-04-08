2a9c9da7e406fa38d80da83d6c43af2b2a2a7ad1
Erika Milevskaya <95300271+rika-milew@users.noreply.github.com>
2025-04-09 00:43:50 +0300

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -4,26 +4,3 @@ $('.header__burger,.header__menu').click(function(event) {
     $('body').toggleClass("lock");  
     })
 });
-
-function printText( el ){
-        
-    let letterTimeout = 120
-
-    let text = el.innerHTML
-    let i = 1
-
-    let print__fn = function(){
-
-            if( i <= text.length ){
-                el.innerHTML = text.substr( 0, i );
-                setTimeout( arguments.callee, letterTimeout );
-            }
-
-            i++;
-        }
-
-    print__fn() 
-};
-
-let el = document.getElementById( 'animation-text' )
-printText( el );
