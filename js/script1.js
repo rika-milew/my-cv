910085b6e421b5b2031afca1313b78601364fcb3
Erika Milevskaya <95300271+rika-milew@users.noreply.github.com>
2025-04-10 00:24:06 +0300

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -4,3 +4,18 @@ $('.header__burger,.header__menu').click(function(event) {
     $('body').toggleClass("lock");  
     })
 });
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
